e419056d84024f9ff18eea16f721e382f8b2a6f8
AthulJIC <athul@jicitsolution.com>
2023-11-08 19:06:14 +0530

    Success screen - ramya

--- App/Screens/forgetpassword/ForgetPasswordScreen.jsx
+++ App/Screens/forgetpassword/ForgetPasswordScreen.jsx
@@ -35,7 +35,7 @@ function ForgetPasswordScreen({route, navigation}){
             <Text style={{fontSize:19,textAlign:'justify',width:'93%',alignSelf:'center',color:'rgba(57, 57, 57, 1)', fontFamily:'Poppins-Regular'}}>Please Connect with the Sales team to {text}</Text>
             <Text style={{color:'rgba(132, 132, 132, 1)', fontSize:16,width:'93%',alignSelf:'center',marginTop:5, fontFamily:'Poppins-Regular'}}>Click to call our sales representative.</Text>
             <View style={styles.modalButtonContainer}>
-                <Pressable style={{ marginBottom: 10, borderRadius: 5, width: '100%', backgroundColor: 'rgba(177, 41, 44, 1)', alignItems: 'center', height: 48, radius: 4, padding: 12 }} onPress={() => makePhoneCall('6380388906')}>
+                <Pressable style={{ marginBottom: 10, borderRadius: 5, width: '100%', backgroundColor: 'rgba(177, 41, 44, 1)', alignItems: 'center', height: 48, radius: 4, padding: 12 }} onPress={() => makePhoneCall('9109176304')}>
                     <Text style={{ fontFamily: 'Poppins-Regular', fontWeight: '500', fontSize: 16, lineHeight: 24, color: '#ffffff', height: 24 }}>
                         Call
                     </Text>
